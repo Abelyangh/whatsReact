26294b3614d8bdb0bab9302d931eb2c8a769a238
sunbin <sunbin>
2019-05-16 14:09:46 +0800

    Add scss file for app

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,9 @@ module.exports = {
             use: {
                 loader: 'babel-loader'
             }
+        }, {
+            test: /.s?css$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
         }]
     }
 }
